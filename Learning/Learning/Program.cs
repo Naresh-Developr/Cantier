c648be1f3545dde940f98f1c7e416370c057ce25
Naresh-Developr <naresh.k.personal@gmail.com>
2025-02-03 09:21:22 +0530

    First web app

---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Learning
+{
+    class Test
+    {
+        public int Method1()
+        {
+            return 2;
+        }
+
+        virtual public void Method2()
+        {
+            Console.WriteLine("hello");
+        }
+
+    }
+        class Program : Test
+        {
+            
+         public override void Method2()
+            {
+                Console.WriteLine("helo");
+            }
+
+            static void Main(string[] args)
+            {
+                Test test = new Test();
+                Console.WriteLine(test.Method1());
+                Program prg = new Program();
+            prg.Method2();
+                        
+                Console.WriteLine("Hello World!");
+            }
+           
+        }
+
+    
+}
+
